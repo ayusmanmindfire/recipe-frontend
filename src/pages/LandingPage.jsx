0a2f8ed0640f8afa64ca7fd2c1c3abba38666e70
Ayusman Parida <ayusmanp@mindfiresolutions.com>
2024-10-14 11:58:57 +0530

    feat: Add landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,34 @@
 import { Navbar } from "../components/Navbar"
+import cookingImage from "../assets/cooking.jpg"
+import { CustomButton } from "../components/CustomButton"
 
-export const LandingPage=()=>{
-    return(
+export const LandingPage = () => {
+    return (
         <>
-        <Navbar/>
-        landingPage
+            <Navbar />
+
+            {/* landing page design */}
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                {/* left container */}
+                <div className="w-full p-10 flex justify-center items-center ">
+                    <div className="block">
+                        <div className="font-bold text-4xl font-Rubik text-primary">
+                            Dive into delicious recipes
+                        </div>
+                        <div className="font-Rubik">
+                            If you are a chef, no matter how good a chef you are, it's not good cooking for yourself; the joy is in cooking for others.
+                        </div>
+                        <div className="flex gap-2">
+                        <CustomButton text="Sign UP"/>
+                        <CustomButton text="Go to recipes"/>
+                        </div>
+                    </div>
+                </div>
+                {/* right container */}
+                <div className="w-full">
+                    <img src={cookingImage} alt="" />
+                </div>
+            </div>
         </>
     )
 }
